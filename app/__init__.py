77dbee14eff1ac562546ea8027fe106bfd8b728c
Ku Hafiq <kuhafiq@yahoo.com>
2025-01-19 21:21:07 +0100

    update app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 import azure.functions as func
 import azure.durable_functions as df
 
-# Define Durable Function App
+# Define Durable Function App BINMOHDN
 myApp = df.DFApp(http_auth_level=func.AuthLevel.ANONYMOUS)
