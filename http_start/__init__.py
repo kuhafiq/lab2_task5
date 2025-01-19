aa51612e8bd498a0e71164402de7b9f868b56007
Ku Hafiq <kuhafiq@yahoo.com>
2025-01-19 23:16:56 +0100

    update

--- http_start/__init__.py
+++ http_start/__init__.py
@@ -3,13 +3,5 @@ import azure.durable_functions as df
 
 async def main(req: func.HttpRequest, starter: str) -> func.HttpResponse:
     client = df.DurableOrchestrationClient(starter)
-    function_name = req.params.get("functionName")
-
-    if not function_name:
-        return func.HttpResponse("Missing 'functionName' in query string", status_code=400)
-
-    try:
-        instance_id = await client.start_new(function_name)
-        return client.create_check_status_response(req, instance_id)
-    except Exception as e:
-        return func.HttpResponse(f"Error starting orchestration: {str(e)}", status_code=500)
+    instance_id = await client.start_new(req.route_params["functionName"])
+    return client.create_check_status_response(req, instance_id)
